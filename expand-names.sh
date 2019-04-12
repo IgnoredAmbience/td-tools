856c707a129937aaac6edc9d5ab40c68416a330a
Thomas Wood <grand.edgemaster@gmail.com>
2019-04-12 23:37:37 +0100

    No longer need to expand filenames

--- expand-names.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Rename all directories to lexically sort
-for d in $(find td -maxdepth 2 -name '?' -type d); do mv $d $(dirname $d)/0$(basename $d); done
-for d in $(find td -maxdepth 3 -name '?' -type d); do mv $d $(dirname $d)/0$(basename $d); done
-for d in $(find td -maxdepth 4 -name '?' -type d); do mv $d $(dirname $d)/0$(basename $d); done
-for f in $(find td -name '?.td'); do mv $f $(dirname $f)/0$(basename $f); done
